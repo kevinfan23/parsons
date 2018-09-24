53aaf927c2e2f5a3fd200527689a860e9da89141
kevinfan23 <kevin.fan23@gmail.com>
2018-09-23 21:35:32 -0400

    finished js week3

--- fall18/js/week3/hw/hw3.js
+++ fall18/js/week3/hw/hw3.js
@@ -2,14 +2,22 @@
 // Using Math.random() function, create random numbers between 1 and 100 and create a simple condition that outputs whether its odd or even,
 
 let num = Math.floor(Math.random() * 101) + 1;
-// output true if even number
-console.log(num%2);
+console.log(oddOrEven(num));
+
+function oddOrEven(num) {
+  return num%2;
+}
 
 // 2. Loops and Maths Operators
 // Loop through numbers 1 to 100 and for each of these numbers, output (console.log()) its square (ie 2 * 2 = 4).
 
-for (let i = 1; i<=100; i++) {
-  console.log(Math.pow(i, 2));
+loopNumbers(1, 100);
+
+function loopNumbers(min, max) {
+  for (let i = min; i<=max; i++) {
+    console.log(Math.pow(i, 2));
+    return;
+  }
 }
 
 // 3. Random and while loop
@@ -17,25 +25,32 @@ for (let i = 1; i<=100; i++) {
 
 let found = false;
 while(!found) {
-  Math.round(Math.random()) ? found = true : found = false;
+  headsOrTails() ? found = true : found = false;
   // console.log(found);
+}
+
+function headsOrTails() {
+  return Math.round(Math.random()) ? 1 : 0;
 }
 
 // 4. Complex Patterns
 // Using the loop approach above, create a chess board using # and space ' '. You could try using loops inside a loop to create the alternative pattern
+printChessBoard();
 
-for (let i = 0; i < 8; i++) {
-  var strline = "";
-  for (let j = 0; j < 8; j++) {
-    if (i%2) {
-      j%2 ? strline += " " : strline += "#";
+function printChessBoard() {
+  for (let i = 0; i < 8; i++) {
+    var strline = "";
+    for (let j = 0; j < 8; j++) {
+      if (i%2) {
+        j%2 ? strline += " " : strline += "#";
+      }
+      else {
+        j%2 ? strline += "#" : strline += " ";
+      }
     }
-    else {
-      j%2 ? strline += "#" : strline += " ";
-    }
+    console.log(strline);
+    strline = "";
   }
-  console.log(strline);
-  strline = "";
 }
 
 // 5. ASCII Art
@@ -43,24 +58,31 @@ for (let i = 0; i < 8; i++) {
 
 // printing a tree...or an arrow...
 let rows = 6;
-let str="";
-for (let i = 1; i <= rows -1; i++) {
-    for (let j = 1; j < rows - i; j++) {
-      str += " ";
-    }
-    let counter = i*2 - 1;
-    while (counter) {
-      str += "*";
-      counter--;
-    }
-    console.log(str);
-    str = "";
-}
+printLeaves(rows);
 
 let trunkRow = 3;
-for (let i = 1; i <= trunkRow; i++) {
-  str = "    *";
-  console.log(str);
+printTrunk(trunkRow);
+
+function printLeaves(height) {
+  let str="";
+  for (let i = 1; i <= rows -1; i++) {
+      for (let j = 1; j < rows - i; j++) {
+        str += " ";
+      }
+      let counter = i*2 - 1;
+      while (counter) {
+        str += "*";
+        counter--;
+      }
+      console.log(str);
+      str = "";
+  }
+}
+
+function printTrunk(height) {
+  for (let i = 1; i <= height; i++) {
+    console.log("    *");
+  }
 }
 
 // output
